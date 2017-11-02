2157f67dac536e12c9a02f30b716bb8c3990928a
Artem Dinaburg <artem@trailofbits.com>
2017-11-02 00:33:35 -0400

    Add an example that modifies an existing C program for symbolic testing

---
+++ examples/Squares.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int square(int x) {
+    return x*x;
+}
+
+#ifdef DEEPSTATE_TEST
+#include <deepstate/DeepState.h>
+DeepState_EntryPoint(test_main) {
+    const char *new_args[2];
+    new_args[0] = "deepstate";
+    new_args[1] = DeepState_CStr(8);
+
+    DeepState_Assert(0 == old_main(2, new_args));
+}
+
+int main(int argc, const char *argv[]) {
+    return 0 == DeepState_Run();
+}
+// yes this is awful but avoids another ifdef
+#define main old_main
+
+#endif
+
+int main(int argc, const char *argv[]) {
+
+    if(argc != 2) {
+        printf("Usage: %s <integer>\n", argv[0]);
+        return -1;
+    }
+    int x = atoi(argv[1]);
+    int y = square(x);
+
+    if(y + 4 == 29) {
+        printf("You found the secret number\n");
+        return 0;
+    } else {
+        printf("Secret NOT found\n");
+        return -1;
+    }
+}
