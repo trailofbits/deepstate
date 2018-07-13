38984fd59fa7ccca4ace5689bd96522b80e407dc
Alex Groce <agroce@gmail.com>
2018-07-13 11:41:48 -0700

    split into three tasks, check OneOf, add fast prime test, try multiple workers

--- tests/test_basic_functionality.py
+++ tests/test_basic_functionality.py
@@ -10,11 +10,32 @@ class TestBasicFunctionality(TestCase):
         if deepstate is None:
             deepstate = "deepstate-angr" # default to angr in an environment without a defined command
 
-        (r, output) = logrun.logrun([deepstate, "build/examples/IntegerArithmetic"],
-                                     "deepstate.out", 1800)
-        self.assertEqual(r, 0)
+        if os.getenv("TASK") is None or os.getenv("TASK") == "PRIMES":
+            (r, output) = logrun.logrun([deepstate, "build/examples/Primes"],
+                                        "deepstate.out", 1800)
+            
+            self.assertEqual(r, 0)
 
-        self.assertTrue("Passed: Arithmetic_AdditionIsCommutative" in output)
-        self.assertTrue("Passed: Arithmetic_AdditionIsAssociative" in output)
-        self.assertTrue("Passed: Arithmetic_InvertibleMultiplication_CanFail" in output)
-        self.assertTrue("Failed: Arithmetic_InvertibleMultiplication_CanFail" in output)                
+            self.assertTrue("Failed: PrimePolynomial_OnlyGeneratesPrimes" in output)
+            self.assertTrue("Failed: PrimePolynomial_OnlyGeneratesPrimes_NoStreaming" in output)
+
+            self.assertTrue("Passed: PrimePolynomial_OnlyGeneratesPrimes" in output)
+            self.assertTrue("Passed: PrimePolynomial_OnlyGeneratesPrimes_NoStreaming" in output)
+
+        if os.getenv("TASK") is None or os.getenv("TASK") == "ONEOF":
+            (r, output) = logrun.logrun([deepstate, "build/examples/OneOf"],
+                                        "deepstate.out", 1800)
+            
+            self.assertEqual(r, 0)
+
+            self.assertTrue("Failed: OneOfExample_ProduceSixtyOrHigher" in output)
+            self.assertTrue("Passed: OneOfExample_ProduceSixtyOrHigher" in output)
+
+        if os.getenv("TASK") is None or os.getenv("TASK") == "ARITHMETIC":
+            (r, output) = logrun.logrun([deepstate, "build/examples/IntegerArithmetic", "--num_workers", "4"],
+                                        "deepstate.out", 1800)
+
+            self.assertTrue("Failed: Arithmetic_InvertibleMultiplication_CanFail" in output)
+            self.assertTrue("Passed: Arithmetic_AdditionIsCommutative" in output)
+            self.assertTrue("Passed: Arithmetic_AdditionIsAssociative" in output)
+            self.assertTrue("Passed: Arithmetic_InvertibleMultiplication_CanFail" in output)
