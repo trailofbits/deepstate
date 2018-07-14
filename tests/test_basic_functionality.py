843608c35424a4c1fcd0a3ffdda4e1ba25d47664
Alex Groce <agroce@gmail.com>
2018-07-14 05:20:11 -0700

    streaming and formatting

--- tests/test_basic_functionality.py
+++ tests/test_basic_functionality.py
@@ -78,7 +78,27 @@ class TestBasicFunctionality(TestCase):
                 self.assertEqual(r, 0)            
 
                 self.assertTrue("Passed: Vector_DoubleReversal" in output)
-                self.assertFalse("Failed: Vector_DoubleReversal" in output)             
+                self.assertFalse("Failed: Vector_DoubleReversal" in output)
+
+            if os.getenv("TASK") is None or os.getenv("TASK") == "STREAMINGANDFORMATTING":
+                (r, output) = logrun.logrun([deepstate, "build/examples/StreamingAndFormatting"],
+                                            "deepstate.out", 1800)
+                #self.assertEqual(r, 0)            
+
+                self.assertTrue("Failed: Streaming_BasicLevels" in output)
+                self.assertTrue("This is a debug message" in output)
+                self.assertTrue("This is an info message" in output)
+                self.assertTrue("This is a warning message" in output)
+                self.assertTrue("This is a error message" in output)
+                self.assertTrue("This is a info message again" in output)
+                self.assertTrue(": 97" in output)
+                self.assertTrue(": 1" in output)
+                self.assertTrue(": 1.000000" in output)
+                self.assertTrue(": string" in output)
+                self.assertTrue(": hello string=world" in output)
+                self.assertTrue(": hello again!" in output)
+                self.assertTrue("Passed: Formatting_OverridePrintf" in output)
+                self.assertFalse("Failed: Formatting_OverridePrintf" in output)                
 
             if os.getenv("TASK") is None or os.getenv("TASK") == "ONEOF":
                 (r, output) = logrun.logrun([deepstate, "build/examples/OneOf"],
