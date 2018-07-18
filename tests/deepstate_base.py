f7013ebad9f063548da0a5e84b67343b96541f0c
Alex Groce <agroce@gmail.com>
2018-07-18 12:14:09 -0700

    add base for all tests

---
+++ tests/deepstate_base.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+from unittest import TestCase
+import logrun
+
+
+class DeepStateTestCase(TestCase):
+  def test_angr(self):
+    self.run_deepstate("deepstate-angr")
+
+  def test_manticore(self):
+    self.run_deepstate("deepstate-manticore")
+
+  def run_deepstate(self, deepstate):
+    print("You should define an actual test of DeepState here.")
