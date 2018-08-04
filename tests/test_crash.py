462fcfb274d3e0fc239b1da9ba5d6826571bf1f0
Alex Groce <agroce@gmail.com>
2018-08-04 13:01:16 -0700

    remove test for replay

--- tests/test_crash.py
+++ tests/test_crash.py
@@ -14,10 +14,5 @@ class CrashTest(deepstate_base.DeepStateTestCase):
     for line in output.split("\n"):
       if ("Saving input to" in line) and (".crash" in line):
         foundCrashSave = True
-        crashLocation = line.split()[-1]
     self.assertTrue(foundCrashSave)
 
-    (r, output) = logrun.logrun(["build/examples/Crash", "--input_test_file", crashLocation],
-                                "deepstate.out", 30)
-    self.assertEqual(r, 0)
-    self.assertTrue("ERROR: Crashed: Crash_SegFault" in output)
