97618c2a24dd432ecb140cc7d423bc17a2cb85d6
Alex Groce <agroce@gmail.com>
2018-09-01 15:53:21 -0700

    start addressing peter's comments

--- bin/deepstate/reducer.py
+++ bin/deepstate/reducer.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python
+# Copyright (c) 2018 Trail of Bits, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import subprocess
+import argparse
 
 def main():
     if "--help" in sys.argv:
@@ -134,8 +150,8 @@ def main():
                     changed = True
                     currentTest = newTest
                     break
-        if not changed:
-            print "NO REDUCTIONS FOUND"
+
+    print "NO REDUCTIONS FOUND"
 
     if (s[1] + 1) > len(currentTest):
         print "PADDING TEST WITH", (s[1] + 1) - len(currentTest), "ZEROS"
