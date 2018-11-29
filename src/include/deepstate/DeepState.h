a3ad1135e7a5afb0cd6ad306a4a46d14f55b4417
Alex Groce <agroce@gmail.com>
2018-11-29 13:25:21 -0700

    also use mod if replaying

--- src/include/deepstate/DeepState.h
+++ src/include/deepstate/DeepState.h
@@ -235,7 +235,8 @@ DEEPSTATE_INLINE static void DeepState_Check(int expr) {
     DEEPSTATE_INLINE static tname DeepState_ ## Tname ## InRange( \
         tname low, tname high) { \
       tname x = DeepState_ ## Tname(); \
-      if (!DeepState_UsingLibFuzzer) \
+      if (!(DeepState_UsingLibFuzzer || HAS_FLAGS_input_test_file \
+	    || HAS_FLAGS_input_test_dir || HAS_FLAGS_input_test_files_dir)) \
         (void) DeepState_Assume(low <= x && x <= high); \
       else \
 	x = low + (x%((high+1)-low)); \
