ecc6652973f485ae7dc4a7733b1c62803c466ede
ggrieco-tob <31542053+ggrieco-tob@users.noreply.github.com>
2018-12-20 17:42:57 -0300

    Update Lists.cpp

--- examples/Lists.cpp
+++ examples/Lists.cpp
@@ -23,12 +23,7 @@ using namespace deepstate;
 
 TEST(Vector, DoubleReversal) {
   ForAll<std::vector<int>>([] (const std::vector<int> &vec1) {
-    std::vector<int> vec2 = vec1;
-    std::reverse(vec2.begin(), vec2.end());
-    if (vec2.size() == 12 && (vec2.front() == vec2.back() % 42))
-       ASSERT(false);
-    std::reverse(vec2.begin(), vec2.end());
-    ASSERT_EQ(vec1, vec2)
-        << "Double reverse of vectors must be equal.";
+    LOG_IF(FATAL, vec1.size() == 12) << "Vector size is: " << vec1.size();
+    ASSERT(true);
   });
 }
