c85124461ae38cba45f8ff0aa9979bcf7eda2aa5
Paweł Płatek <e2.8a.95@gmail.com>
2020-02-14 11:38:55 +0100

    fix symex tests (logrun)

--- tests/logrun.py
+++ tests/logrun.py
@@ -2,15 +2,31 @@ from __future__ import print_function
 import subprocess
 import time
 import sys
+from tempfile import mkdtemp
+from shutil import rmtree
 
-def logrun(cmd, file, timeout):
+
+def logrun(cmd, file, timeout, break_callback=None):
   sys.stderr.write("\n\n" + ("=" * 80) + "\n")
   sys.stderr.write("RUNNING: ")
   sys.stderr.write(" ".join(cmd) + "\n\n")
   sys.stderr.flush()
+
+  tmp_out_dir = None
   with open(file, 'w') as outf:
+    additional_args = []
+
+    # auto-create output dir
+    if set(cmd).isdisjoint({"-o", "--output_test_dir", "--out_test_name"}):
+      tmp_out_dir = mkdtemp(prefix="deepstate_logrun_")
+      additional_args.extend(["--output_test_dir", tmp_out_dir])  # create empty output dir
+
     # We need to set log_level so we see ALL messages, for testing
-    p = subprocess.Popen(cmd + ["--min_log_level", "0"], stdout=outf, stderr=outf)
+    if "--min_log_level" not in cmd:
+      additional_args.extend(["--min_log_level", "0"])
+
+    p = subprocess.Popen(cmd + additional_args, stdout=outf, stderr=outf)
+
   start = time.time()
   oldContents = ""
   lastOutput = time.time()
@@ -19,16 +35,24 @@ def logrun(cmd, file, timeout):
       sys.stderr.write(".")
       sys.stderr.flush()
       lastOutput = time.time()
+
     with open(file, 'r') as inf:
       contents = inf.read()
+
     if len(contents) > len(oldContents):
       sys.stderr.write(contents[len(oldContents):])
       sys.stderr.flush()
       oldContents = contents
       lastOutput = time.time()
+
+    if break_callback and break_callback(contents):
+      break
+
     time.sleep(0.05)
+
   totalTime = time.time() - start
   sys.stderr.write("\n")
+
   rv = (p.returncode, contents)
   if p.poll() is None:
     rv = ("TIMEOUT", contents)
@@ -36,13 +60,14 @@ def logrun(cmd, file, timeout):
     rv = ("EXCEPTION RAISED", contents)
   if "internal error" in contents:
     rv = ("INTERNAL ERROR", contents)
+
   sys.stderr.write("\nDONE\n\n")
   sys.stderr.write("TOTAL EXECUTION TIME: " + str(totalTime) + "\n")
   sys.stderr.write("RETURN VALUE: " + str(p.returncode) + "\n")
   sys.stderr.write("RETURNING AS RESULT: " + str(rv[0]) + "\n")
   sys.stderr.write("=" * 80 + "\n")
 
+  if tmp_out_dir:
+    rmtree(tmp_out_dir, ignore_errors=True)
+
   return rv
-
-
-
