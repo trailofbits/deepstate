18f16628295601709bf6a2783d3fb958d29a81e9
Bob Costas <finkzane@gmail.com>
2020-03-05 21:12:46 -0700

    added nextInt example

--- src/lib/GenTestBridge.cpp
+++ src/lib/GenTestBridge.cpp
@@ -1,4 +1,6 @@
 #include "deepstate/GenTestBridge.h"
+#include "BinaryParser.h"
+#include "BinaryIterator.h"
 #include <iostream>
 #include <string>
 
@@ -8,5 +10,12 @@ int DeepStateCreateStandalone( const char *output_filename,
                                const char *translation_config_filename
                              )
 {
+    BinaryParser b;
+    b.parse( std::string( binary_filename ) );
+    auto iter = b.getIterator();
+    int n = iter.nextInt();
+
+    std::cout << n  << std::endl;
+    std::cout << n  + n << std::endl;
     std::cout << std::string( input_source_filename ) << std::endl;
 }
