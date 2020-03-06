b98aac39e36bb87e631669677834c88dbc921a14
Bob Costas <finkzane@gmail.com>
2020-03-05 17:35:49 -0700

    added bridge to GenTestSoftware

---
+++ src/lib/GenTestBridge.cpp
@@ -0,0 +1,12 @@
+#include "deepstate/GenTestBridge.h"
+#include <iostream>
+#include <string>
+
+int DeepStateCreateStandalone( const char *output_filename,
+                               const char *input_source_filename,
+                               const char *binary_filename,
+                               const char *translation_config_filename
+                             )
+{
+    std::cout << std::string( input_source_filename ) << std::endl;
+}
