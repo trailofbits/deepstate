ffd327ceb9cafb0a7971966050b925abb2292c35
Alex Groce <agroce@gmail.com>
2021-02-22 06:22:44 -0700

    libfuzzer output

--- src/lib/Log.c
+++ src/lib/Log.c
@@ -103,7 +103,7 @@ void DeepState_LogVFormat(enum DeepState_LogLevel level,
                           const char *format, va_list args) {
   struct DeepState_VarArgs va;
   va_copy(va.args, args);
-  if (DeepState_UsingLibFuzzer && (level < DeepState_LogExternal)) {
+  if (DeepState_UsingLibFuzzer && (level != DeepState_LogDebug)) {
     return;
   }
   DeepState_LogStream(level);
