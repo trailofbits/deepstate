df254d13a5a58f1a77d948ee08f1ae11e2e81870
Alex Groce <agroce@gmail.com>
2021-02-26 14:38:35 -0700

    Ten seconds is plenty

--- tests/test_sanity_check.py
+++ tests/test_sanity_check.py
@@ -11,7 +11,7 @@ class SanityCheck(deepstate_base.DeepStateTestCase):
     os.mkdir("OneOf_out")
     (r, output) = logrun.logrun(["build/examples/OneOf",
                                   "--fuzz",
-                                  "--timeout", "30",
+                                  "--timeout", "",
                                   "--no_fork",
                                   "--output_test_dir", "OneOf_out",
                                   "--min_log_level", "2",
